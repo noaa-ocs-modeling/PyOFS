2b1c82c375a65a91765fd81d7440da4c7b2f8e91
Zach Burnett <zachary.burnett@noaa.gov>
2018-09-19 10:10:26 -0400

    fixed incorrect indent, added delete statements, refactored

--- main/write_daily_average.py
+++ main/write_daily_average.py
@@ -22,104 +22,113 @@ LOG_DIR = os.path.join(DATA_DIR, 'log')
 
 JSON_PATH = os.path.join(DATA_DIR, r'reference\model_dates.json')
 OUTPUT_DIR = os.path.join(DATA_DIR, 'output')
-DAILY_AVERAGE_DIR = os.path.join(OUTPUT_DIR, 'daily_averages')
+DAILY_AVERAGES_DIR = os.path.join(OUTPUT_DIR, 'daily_averages')
 
 # default nodata value used by leaflet-geotiff renderer
 LEAFLET_NODATA_VALUE = -9999
 
 
-def write_daily_average(output_dir, current_model_run_date, day_deltas, log_path):
+def write_daily_average(output_dir, model_run_date: datetime.date, day_deltas, log_path):
+    if type(model_run_date) == 'datetime.Date':
+        model_run_date = datetime.datetime.combine(model_run_date, datetime.datetime.min.time())
+
     # define directories to which output rasters will be written
-    output_dirs = {current_day_delta: os.path.join(output_dir, (
-            current_model_run_date + datetime.timedelta(days=current_day_delta)).strftime("%Y%m%d")) for
-                   current_day_delta in day_deltas}
+    output_dirs = {
+        day_delta: os.path.join(output_dir, (model_run_date + datetime.timedelta(days=day_delta)).strftime("%Y%m%d"))
+        for day_delta in day_deltas}
 
-    for current_day_delta, current_dir in output_dirs.items():
+    for day_delta, daily_average_dir in output_dirs.items():
         # ensure output directory exists
-        if not os.path.isdir(current_dir):
-            os.mkdir(current_dir)
+        if not os.path.isdir(daily_average_dir):
+            os.mkdir(daily_average_dir)
 
-        current_start_time = datetime.datetime.now()
+        start_time = datetime.datetime.now()
 
-        current_date = current_model_run_date + datetime.timedelta(days=current_day_delta)
-        next_date = current_date + datetime.timedelta(days=1)
+        start_datetime = model_run_date + datetime.timedelta(days=day_delta)
+        end_datetime = start_datetime + datetime.timedelta(days=1)
 
-        if current_day_delta <= 0:
+        if day_delta <= 0:
             try:
-                # print(f'Processing HFR for {current_date}')
-                current_hfr = dataset.hfr.HFR_Range(current_date, next_date)
-                current_hfr.write_rasters(current_dir, filename_suffix=f'{current_date.strftime("%Y%m%d")}',
-                                          variables=['u', 'v'], vector_components=True, drivers=['AAIGrid'],
-                                          fill_value=LEAFLET_NODATA_VALUE)
+                # print(f'Processing HFR for {start_datetime}')
+                hfr_range = dataset.hfr.HFR_Range(start_datetime, end_datetime)
+                hfr_range.write_rasters(daily_average_dir, filename_suffix=f'{start_datetime.strftime("%Y%m%d")}',
+                                        variables=['u', 'v'], vector_components=True, drivers=['AAIGrid'],
+                                        fill_value=LEAFLET_NODATA_VALUE)
+                del hfr_range
             except dataset._utilities.NoDataError as error:
                 with open(log_path, 'a') as log_file:
                     log_file.write(f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")} (0.00s): {error}\n')
                     print(error)
 
             try:
-                # print(f'Processing VIIRS for {current_date}')
-                current_viirs = dataset.viirs.VIIRS_Range(current_date, next_date)
-                current_viirs.write_raster(current_dir,
-                                           filename_prefix=f'viirs_sst_{current_date.strftime("%Y%m%d")}_morning',
-                                           start_datetime=current_date, end_datetime=current_date.replace(hour=6),
-                                           drivers=['GTiff'], fill_value=LEAFLET_NODATA_VALUE)
-                current_viirs.write_raster(current_dir,
-                                           filename_prefix=f'viirs_sst_{current_date.strftime("%Y%m%d")}_daytime',
-                                           start_datetime=current_date.replace(hour=6),
-                                           end_datetime=current_date.replace(hour=18), drivers=['GTiff'],
-                                           fill_value=LEAFLET_NODATA_VALUE)
-                current_viirs.write_raster(current_dir,
-                                           filename_prefix=f'viirs_sst_{current_date.strftime("%Y%m%d")}_evening',
-                                           start_datetime=current_date.replace(hour=18), end_datetime=next_date,
-                                           drivers=['GTiff'], fill_value=LEAFLET_NODATA_VALUE)
+                # print(f'Processing VIIRS for {start_datetime}')
+                viirs_range = dataset.viirs.VIIRS_Range(start_datetime, end_datetime)
+
+                morning_datetime = start_datetime + datetime.timedelta(hours=6)
+                evening_datetime = start_datetime + datetime.timedelta(hours=18)
+
+                viirs_range.write_raster(daily_average_dir,
+                                         filename_prefix=f'viirs_sst_{start_datetime.strftime("%Y%m%d")}_morning',
+                                         start_datetime=start_datetime, end_datetime=morning_datetime,
+                                         drivers=['GTiff'], fill_value=LEAFLET_NODATA_VALUE)
+                viirs_range.write_raster(daily_average_dir,
+                                         filename_prefix=f'viirs_sst_{start_datetime.strftime("%Y%m%d")}_daytime',
+                                         start_datetime=morning_datetime, end_datetime=evening_datetime,
+                                         drivers=['GTiff'], fill_value=LEAFLET_NODATA_VALUE)
+                viirs_range.write_raster(daily_average_dir,
+                                         filename_prefix=f'viirs_sst_{start_datetime.strftime("%Y%m%d")}_evening',
+                                         start_datetime=evening_datetime, end_datetime=end_datetime, drivers=['GTiff'],
+                                         fill_value=LEAFLET_NODATA_VALUE)
+                del viirs_range
             except dataset._utilities.NoDataError as error:
                 with open(log_path, 'a') as log_file:
                     log_file.write(f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")} (0.00s): {error}\n')
                     print(error)
 
-            # only retrieve forecasts that have not already been written
-            if current_day_delta == -1:
-                current_wcofs_time_indices = [-1]
-            else:
-                current_wcofs_time_indices = [current_day_delta + 1]
+        # only retrieve forecasts that have not already been written
+        if day_delta == -1:
+            wcofs_time_indices = [-1]
+        else:
+            wcofs_time_indices = [day_delta + 1]
 
-            try:
-                # print(f'Processing WCOFS for {current_date}')
-                current_wcofs = dataset.wcofs.WCOFS_Range(current_date, next_date, source='avg',
-                                                          time_indices=current_wcofs_time_indices)
-                current_wcofs.write_rasters(current_dir, ['temp'], drivers=['GTiff'], fill_value=LEAFLET_NODATA_VALUE)
-                current_wcofs.write_rasters(current_dir, ['u', 'v'], vector_components=True, drivers=['AAIGrid'],
-                                            fill_value=LEAFLET_NODATA_VALUE)
-            except dataset._utilities.NoDataError as error:
-                with open(log_path, 'a') as log_file:
-                    log_file.write(f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")} (0.00s): {error}\n')
-                    print(error)
-
+        try:
+            # print(f'Processing WCOFS for {date}')
+            wcofs_range = dataset.wcofs.WCOFS_Range(start_datetime, end_datetime, source='avg',
+                                                    time_indices=wcofs_time_indices)
+            wcofs_range.write_rasters(daily_average_dir, ['temp'], drivers=['GTiff'], fill_value=LEAFLET_NODATA_VALUE)
+            wcofs_range.write_rasters(daily_average_dir, ['u', 'v'], vector_components=True, drivers=['AAIGrid'],
+                                      fill_value=LEAFLET_NODATA_VALUE)
+            del wcofs_range
+        except dataset._utilities.NoDataError as error:
             with open(log_path, 'a') as log_file:
-                message = f'Wrote files to {current_dir}'
-                log_file.write(
-                        f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")} ({(datetime.datetime.now() - current_start_time).total_seconds():.2f}s): {message}\n')
-                print(message)
+                log_file.write(f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")} (0.00s): {error}\n')
+                print(error)
 
-            current_start_time = datetime.datetime.now()
+        with open(log_path, 'a') as log_file:
+            message = f'Wrote files to {daily_average_dir}'
+            log_file.write(
+                    f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")} ({(datetime.datetime.now() - start_time).total_seconds():.2f}s): {message}\n')
+            print(message)
 
-            # populate JSON file with new directory structure so that JavaScript application can see it
-            json_dir_structure.populate_json(OUTPUT_DIR, JSON_PATH)
+        start_time = datetime.datetime.now()
 
-            with open(log_path, 'a') as log_file:
-                message = f'Updated directory structure at {JSON_PATH}'
-                log_file.write(
-                        f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")} ({(datetime.datetime.now() - current_start_time).total_seconds():.2f}s): {message}\n')
-                print(message)
+        # populate JSON file with new directory structure so that JavaScript application can see it
+        json_dir_structure.populate_json(OUTPUT_DIR, JSON_PATH)
+
+        with open(log_path, 'a') as log_file:
+            message = f'Updated directory structure at {JSON_PATH}'
+            log_file.write(
+                    f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")} ({(datetime.datetime.now() - start_time).total_seconds():.2f}s): {message}\n')
+            print(message)
 
 
 if __name__ == '__main__':
     start_time = datetime.datetime.now()
 
     # create folders if they do not exist
-    for current_dir in [OUTPUT_DIR, DAILY_AVERAGE_DIR, LOG_DIR]:
-        if not os.path.isdir(current_dir):
-            os.mkdir(current_dir)
+    for daily_average_dir in [OUTPUT_DIR, DAILY_AVERAGES_DIR, LOG_DIR]:
+        if not os.path.isdir(daily_average_dir):
+            os.mkdir(daily_average_dir)
 
     # define log filename
     log_path = os.path.join(LOG_DIR, f'{datetime.datetime.now().strftime("%Y%m%d")}_daily_average.log')
@@ -132,15 +141,15 @@ if __name__ == '__main__':
         print(message)
 
     # get current date
-    model_run_dates = [start_time.replace(hour=0, minute=0, second=0, microsecond=0)]
-    # model_run_dates = dataset._utilities.day_range(datetime.datetime(2018, 8, 27),
-    #                                                datetime.datetime.now() + datetime.timedelta(days=1))
+    # model_run_dates = [start_time.replace(hour=0, minute=0, second=0, microsecond=0)]
+    model_run_dates = dataset._utilities.day_range(datetime.datetime(2018, 9, 16),
+                                                   datetime.datetime.now() + datetime.timedelta(days=1))
 
     # define dates over which to collect data (dates after today are for WCOFS forecast)
     day_deltas = [-1, 0, 1, 2]
 
-    for current_model_run_date in model_run_dates:
-        write_daily_average(os.path.join(DATA_DIR, DAILY_AVERAGE_DIR), current_model_run_date, day_deltas, log_path)
+    for model_run_date in model_run_dates:
+        write_daily_average(os.path.join(DATA_DIR, DAILY_AVERAGES_DIR), model_run_date, day_deltas, log_path)
 
     with open(log_path, 'a') as log_file:
         message = f'Finished writing files. Total time: {(datetime.datetime.now() - start_time).total_seconds():.2f} seconds'
