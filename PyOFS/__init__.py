ed43f6eda7369b038e4dbad41620b657910e3988
zacharyburnett <zrb@umd.edu>
2019-03-14 12:10:28 -0400

    remove path suggestions in favor of batch file

--- PyOFS/__init__.py
+++ PyOFS/__init__.py
@@ -1,8 +1,5 @@
 import os
 
-# PATH=D:\Python\conda\GIS;D:\Python\conda\GIS\lib\site-packages;D:\Python\conda\GIS\bin;D:\Python\conda\GIS\Scripts;D:\Python\conda\GIS\Library\bin;D:\Python\conda\GIS\Library\usr\bin;D:\Python\conda\GIS\Library\mingw-w64\bin
-# GDAL_DATA=D:\Python\conda\GIS\Library\share\gdal
-
 CRS_EPSG = 4326
 
 DATA_DIR = os.environ['OFS_DATA']
