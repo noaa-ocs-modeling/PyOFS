bfe29376e7b1ed2d3ae6b70ab282ead514aab4d5
zacharyburnett <zrb@umd.edu>
2019-03-14 12:12:42 -0400

    develop changes (paths and stuff)

--- PyOFS/__init__.py
+++ PyOFS/__init__.py
@@ -1,11 +1,8 @@
 import os
 
-# PATH=D:\Python\PyOFS_develop\PyOFS\dataset;D:\Python\PyOFS_develop;D:\Python\conda\GIS\python37.zip;D:\Python\conda\GIS\DLLs;D:\Python\conda\GIS\lib;D:\Python\conda\GIS;D:\Python\conda\GIS\lib\site-packages;D:\Python\conda\GIS\bin;D:\Python\conda\GIS\Scripts;D:\Python\conda\GIS\Library\bin;D:\Python\conda\GIS\Library\usr\bin;D:\Python\conda\GIS\Library\mingw-w64\bin
-# GDAL_DATA=D:\Python\conda\GIS\Library\share\gdal
-
 CRS_EPSG = 4326
 
-DATA_DIR = os.environ['OFS_DATA']
-
-# for development branch
-# DATA_DIR = os.path.join(DATA_DIR, 'develop')
+if 'OFS_DATA' in os.environ:
+    DATA_DIR = os.environ['OFS_DATA']
+else:
+    DATA_DIR = r"D:\data"
