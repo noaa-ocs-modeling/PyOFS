973175d31d1af9cac82487743b4debad2cb0917c
zacharyburnett <zrb@umd.edu>
2019-03-14 12:01:52 -0400

    fi9x

--- PyOFS/__init__.py
+++ PyOFS/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-# PATH=D:\Python\PyOFS_develop\PyOFS\dataset;D:\Python\PyOFS_develop;D:\Python\conda\GIS\python37.zip;D:\Python\conda\GIS\DLLs;D:\Python\conda\GIS\lib;D:\Python\conda\GIS;D:\Python\conda\GIS\lib\site-packages;D:\Python\conda\GIS\bin;D:\Python\conda\GIS\Scripts;D:\Python\conda\GIS\Library\bin;D:\Python\conda\GIS\Library\usr\bin;D:\Python\conda\GIS\Library\mingw-w64\bin
+# PATH=D:\Python\conda\GIS;D:\Python\conda\GIS\lib\site-packages;D:\Python\conda\GIS\bin;D:\Python\conda\GIS\Scripts;D:\Python\conda\GIS\Library\bin;D:\Python\conda\GIS\Library\usr\bin;D:\Python\conda\GIS\Library\mingw-w64\bin
 # GDAL_DATA=D:\Python\conda\GIS\Library\share\gdal
 
 CRS_EPSG = 4326
