98f9b47f3bf24f369ef47d2a713a8f10efe510fc
Zach Burnett <zachary.burnett@noaa.gov>
2019-05-03 14:46:07 -0400

    explicit layer name

--- main/tracking/particle_contour.py
+++ main/tracking/particle_contour.py
@@ -696,7 +696,8 @@ if __name__ == '__main__':
 
     print(f'[{datetime.datetime.now()}]: Started processing...')
 
-    with fiona.open(os.path.join(DATA_DIR, 'reference', 'study_points.gpkg')) as contour_centers_file:
+    with fiona.open(os.path.join(DATA_DIR, 'reference', 'study_points.gpkg'),
+                    layer='study_points') as contour_centers_file:
         for point in contour_centers_file:
             contour_id = int(point['id'])
             contour_centers[contour_id] = point['geometry']['coordinates']
